bbec8a648ac4753812e836fc2d93844ee376e8f6
Denlz Polat <91384872+DenizzPolatt@users.noreply.github.com>
2023-01-07 00:20:22 +0300

    Multiple Spawns
    Multiple prefabs are instantiated at random positions

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -7,15 +7,30 @@ public class Spawner : MonoBehaviour
     public GameObject objectToSpawn;
     public Vector3 minPosition;
     public Vector3 maxPosition;
+
+    private int numberOfSpawns = 5;
     
     void Start()
+    {
+        spawnFood();
+    }
+
+    void spawnFood()
+    {
+        for (int i = 0; i < numberOfSpawns; i++)
+        {
+            Instantiate(objectToSpawn, randomPosition(), Quaternion.identity);
+        }
+    }
+
+    private Vector3 randomPosition()
     {
         Vector3 randomPosition = new Vector3(
             Random.Range(minPosition.x, maxPosition.x),
             Random.Range(minPosition.y, maxPosition.y),
             Random.Range(minPosition.z, maxPosition.z)
         );
-        
-        Instantiate(objectToSpawn, randomPosition, Quaternion.identity);
+
+        return randomPosition;
     }
 }
